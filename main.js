3a733367a9fe8a8ed9c2bc0f83b473e1bb2829ef
Brandon Wilmott <Bran@Brandons-MacBook.local>
2017-10-03 17:07:12 -0500

    day 2 lab

---
+++ main.js
@@ -0,0 +1,40 @@
+//declare friends and song count down
+var friends = ["Pedro","Kyle","Shantal","Shane","Andrew"];
+
+
+
+
+//for loop for friends
+for (var i = 0; i < friends.length ; i++){
+        console.log(" " +friends[i]);
+        var lines = 99
+
+//while loop for countdown
+
+
+
+while (lines > 1){
+   
+
+    console.log(lines+" lines of code in the file, "+lines+" lines of code "+friends[i]+" strikes one out clears it all out "+(lines-1)+" lines of code in the file ");
+    lines--;
+
+  
+}
+
+if (lines = 1 ){
+    console.log(+lines+" lines of code in the file, "+lines+" lines of code "+friends[i]+" strikes one out clears it all out no more lines of code in the file "); 
+
+}
+
+}
+
+
+
+
+
+
+
+
+
+
